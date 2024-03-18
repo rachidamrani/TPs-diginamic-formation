f1cc8808359f5076c981d12ef3f5955d1300807b
rachidmarani <rachid3alaoui@gmail.com>
2024-03-18 11:10:45 +0100

    Added new C# project

---
+++ TP-Recursive-method-list-folders-subfolders-files/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+DirectoryInfo directoryInfo = new DirectoryInfo(@"D:/Formation Diginamic");
+
+listFolderFiles(directoryInfo, 1);
+
+void listFolderFiles(DirectoryInfo directoryInfo, int level)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{new string('.', level)}{directoryInfo.Name}");
+
+    listFiles(directoryInfo,level);
+
+    foreach (DirectoryInfo directory in directoryInfo.GetDirectories())
+    {
+        listFiles(directory, level);
+        listFolderFiles(directory, ++level);
+    }
+}
+
+
+void listFiles(DirectoryInfo directoryInfo, int level)
+{
+    level++;
+
+    Console.ForegroundColor = ConsoleColor.Blue;
+
+    FileInfo[]? subFiles = directoryInfo.GetFiles();
+
+    foreach (var file in subFiles)
+    {
+        Console.WriteLine($"{new string('.', level)}{file.Name}");
+    }
+}
+
+
+
+
+
